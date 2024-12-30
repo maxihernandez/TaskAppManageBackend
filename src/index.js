006e0f5a8550be60a7cf60a9bb3e42664d7b90eb
MaxiHernandez <maxi-hernandez@hotmail.com>
2024-12-30 12:35:31 -0300

    api-docs

--- src/index.js
+++ src/index.js
@@ -29,5 +29,5 @@ app.get('/', (req, res) => {
 // Inicia el servidor en el puerto 3000
 const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Servidor corriendo en http://localhost:${PORT}/api-docs`);
 });
